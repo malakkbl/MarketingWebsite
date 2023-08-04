b197521171f2ffa8ea58a337f1450af6ccdfe097
unknown <malak012426@gmail.com>
2023-08-04 16:38:17 +0100

    updated homepage

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,13 @@
 //Footer date :
 document.getElementById('footer-text').textContent = '©' + new Date().getFullYear() + ' Made by Malak Kably. All rights reserved.';
 
+// Hover for the homepage img :
+var image = document.getElementById('home_icon');
+var defaultSource = image.src;
+var hoverSource = "/img/GroovingBlack.png";
+image.addEventListener('mouseover', function() {
+    image.src = hoverSource;
+});
+image.addEventListener('mouseout', function() {
+    image.src = defaultSource;
+});
