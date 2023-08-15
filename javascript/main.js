8555c1b7c1e61b47711c7b92802f598249d7ea98
unknown <malak012426@gmail.com>
2023-08-15 22:21:41 +0100

    homepage upadte

--- javascript/main.js
+++ javascript/main.js
@@ -11,3 +11,29 @@ image.addEventListener('mouseover', function() {
 image.addEventListener('mouseout', function() {
     image.src = defaultSource;
 });
+
+// Countdown :
+// Set the date we're counting down to
+var countDownDate = new Date("Dec 31, 2023 23:59:59").getTime();
+
+// Update the countdown every second
+var countdownInterval = setInterval(function() {
+    var now = new Date().getTime();
+    var timeLeft = countDownDate - now;
+    
+    // Calculate hours, minutes, and seconds
+    var hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+    // Display the result in respective elements
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+    // If the countdown is finished, display some text
+    if (timeLeft < 0) {
+        clearInterval(countdownInterval);
+        document.getElementById("countdown").innerHTML = "EXPIRED";
+    }
+}, 1000);
