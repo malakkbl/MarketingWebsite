735baf8f020838e28853d3c2e689c2e3038866dc
unknown <malak012426@gmail.com>
2023-08-16 12:04:01 +0100

    clean about us

--- javascript/main.js
+++ javascript/main.js
@@ -1,15 +1,16 @@
 //Footer date :
-document.getElementById('footer-text').textContent = '©' + new Date().getFullYear() + ' Made by Malak Kably. All rights reserved.';
+document.getElementById("footer-text").textContent =
+  "©" + new Date().getFullYear() + " Made by Malak Kably. All rights reserved.";
 
 // Hover for the homepage img :
-var image = document.getElementById('home_icon');
+var image = document.getElementById("home_icon");
 var defaultSource = image.src;
 var hoverSource = "/img/GroovingBlack.png";
-image.addEventListener('mouseover', function() {
-    image.src = hoverSource;
+image.addEventListener("mouseover", function () {
+  image.src = hoverSource;
 });
-image.addEventListener('mouseout', function() {
-    image.src = defaultSource;
+image.addEventListener("mouseout", function () {
+  image.src = defaultSource;
 });
 
 // COUNTDOWN :
@@ -17,23 +18,23 @@ image.addEventListener('mouseout', function() {
 var countDownDate = new Date("Dec 31, 2023 23:59:59").getTime();
 
 // Update the countdown every second
-var countdownInterval = setInterval(function() {
-    var now = new Date().getTime();
-    var timeLeft = countDownDate - now;
-    
-    // Calculate hours, minutes, and seconds
-    var hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+var countdownInterval = setInterval(function () {
+  var now = new Date().getTime();
+  var timeLeft = countDownDate - now;
 
-    // Display the result in respective elements
-    document.getElementById("hours").innerHTML = hours;
-    document.getElementById("minutes").innerHTML = minutes;
-    document.getElementById("seconds").innerHTML = seconds;
+  // Calculate hours, minutes, and seconds
+  var hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
 
-    // If the countdown is finished, display some text
-    if (timeLeft < 0) {
-        clearInterval(countdownInterval);
-        document.getElementById("countdown").innerHTML = "EXPIRED";
-    }
+  // Display the result in respective elements
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+
+  // If the countdown is finished, display some text
+  if (timeLeft < 0) {
+    clearInterval(countdownInterval);
+    document.getElementById("countdown").innerHTML = "EXPIRED";
+  }
 }, 1000);
