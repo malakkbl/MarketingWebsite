ce3257f0f624d9f3661bbba80a3a7a2b29549a3c
unknown <malak012426@gmail.com>
2023-08-24 21:56:13 +0100

    update

--- javascript/contact.js
+++ javascript/contact.js
@@ -3,8 +3,10 @@ document.getElementById("footer-text").textContent =
   "©" + new Date().getFullYear() + " Made by Malak Kably. All rights reserved.";
 
 // Contact form :
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-    event.preventDefault();  
+document
+  .getElementById("contactForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
     alert("Thanks for reaching out! We'll dance back to you soon.");
     event.target.reset();
-});
+  });
